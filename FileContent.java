549677eaadd95939b3d80236995f3232210ba04d
Kesavan1112 <149461220+Kesavan1112@users.noreply.github.com>
2024-10-06 09:53:37 +0530

    Add files via upload

--- FileContent.java
+++ FileContent.java
@@ -1,39 +1,40 @@
 import java.util.Scanner;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.path;
-import java.nio.file.paths;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-public class FileContent
-{
-public static void main(String[]args)
-{
-String fname;
-Scanner scan=new Scanner(System.in);
-System.out.print("Enter File Name to open (with extension like file.txt):");
-fname=scan.nextLine();
-String line=null;
-try{
-FileReader fr=new FileReader(fname);
-BufferedReader bufferedReader=new BufferedReader(fileReader);
-while(line = bufferedReader.readLine())!=null)
-{
-System.out.println(line);
-}
-Path filepath=Paths.get(fname);
-long filesiz=Files.size(filepath);
 
-Path file=paths.get(fname);
-BasicFileAttributes attr=Files.readAttributes(file,BasicFileAttributes.class);
-System.out.format("the size of the file:%d byte",fileSize);
-System.out.println("\ncreationTime:"+attr.creationTime());
-bufferedReader.close();
-}
+public class FileContent {
+    public static void main(String[] args) {
+        String fname;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter File Name to open (with extension like file.txt): ");
+        fname = scan.nextLine();
+        String line = null;
 
-catch(IOException ex)
-{
-System.out.println("error in reading file named'"+fname+"");
-}}
-}
- 
+        try {
+            // File reading setup
+            FileReader fr = new FileReader(fname);
+            BufferedReader bufferedReader = new BufferedReader(fr);
+            
+            // Reading file line by line
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
 
+            // Get file path and size
+            Path filepath = Paths.get(fname);
+            long fileSize = Files.size(filepath);
+
+            // Get file attributes (size, creation time, etc.)
+            BasicFileAttributes attr = Files.readAttributes(filepath, BasicFileAttributes.class);
+            System.out.format("The size of the file: %d bytes\n", fileSize);
+            System.out.println("Creation Time: " + attr.creationTime());
+
+            // Close the bufferedReader
+            bufferedReader.close();
+
+        } catch (IOException ex) {
+            System.out.println("Error in reading file named '" + fname + "'");
+        }
+    }
+}
